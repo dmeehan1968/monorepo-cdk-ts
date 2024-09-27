56f623ef407662c46e97ce3e9c973d2a487f129e
Dave Meehan <dave_meehan@replicated.co.uk>
2024-09-27 16:21:07 +0100

    Move the nodejs test to MyFunction

--- packages/app/src/stacks/MyStack.test.ts
+++ packages/app/src/stacks/MyStack.test.ts
@@ -1,19 +1,18 @@
-import { App } from 'aws-cdk-lib';
+import { App, Stack } from 'aws-cdk-lib';
 import { MyStack } from './MyStack.js'
-import { Template } from 'aws-cdk-lib/assertions';
 import * as cdkContext from '../../cdk.json'
+import { MyFunction } from '@monorepo/my-function'
 
 describe('MyStack', () => {
-  it('should have a test', () => {
-    const app = new App({ context: { ...cdkContext, 'aws:cdk:bundling-stacks': [] }})
-    const stack = new MyStack(app, 'MyStack')
-    const template = Template.fromStack(stack)
 
-    template.hasResource('AWS::Lambda::Function', {
-      Properties: {
-        Handler: 'index.handler',
-        Runtime: 'nodejs20.x',
-      }
-    })
+  let sut: Stack
+
+  beforeAll(() => {
+    const app = new App({ context: { ...cdkContext, 'aws:cdk:bundling-stacks': [] }})
+    sut = new MyStack(app, 'MyStack')
+  })
+
+  it('should have a MyFunction construct', () => {
+    expect(sut.node.findChild('MyFunction')).toBeInstanceOf(MyFunction)
   })
 });
---
+++ packages/my-function/src/infra/MyFunction.test.ts
@@ -0,0 +1,32 @@
+import { App, Stack } from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import { MyFunction } from './MyFunction.js'
+import { jest } from '@jest/globals'
+
+describe('MyStack', () => {
+
+  let sut: Template
+
+  beforeAll(() => {
+    const app = new App({ context: { 'aws:cdk:bundling-stacks': [] } })
+    const stack = new Stack(app, 'Default')
+    new MyFunction(stack, 'MyFunction')
+    sut = Template.fromStack(stack)
+  })
+
+  it('should have a lambda', () => {
+    sut.resourceCountIs('AWS::Lambda::Function', 1)
+  })
+
+  it('should use node 20', () => {
+    sut.hasResourceProperties('AWS::Lambda::Function', {
+      Runtime: 'nodejs20.x',
+    })
+  })
+
+  it('should use handler', () => {
+    sut.hasResourceProperties('AWS::Lambda::Function', {
+      Handler: 'index.handler',
+    })
+  })
+});
